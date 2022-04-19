3b82c1d77198967e85d166faa98fe1b9ce686961
curlyjun <tjdwns4850@gmail.com>
2022-04-19 19:29:34 +0900

    feat: 프로젝트 기본 세팅

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+export default GlobalStyle;
