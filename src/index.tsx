55a0123a86aa687776906b4664fe99b096f8ed04
curlyjun <tjdwns4850@gmail.com>
2022-04-29 00:43:15 +0900

    feat: Create InitProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
-import { theme } from '@styles/theme';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import axios from 'axios';
+import { theme } from '@styles/theme';
+import { AuthProvider } from '@hooks/useAuth';
+import GlobalStyle from '@styles/globalStyle';
+import InitProvider from '@components/initProvider/InitProvider';
 import App from './App';
-import GlobalStyle from './styles/globalStyle';
+
+/** @todo edit to server url */
+axios.defaults.baseURL = 'http://localhost:3000';
+/** @description for refreshToken */
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -14,8 +22,13 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <InitProvider>
+            <App />
+          </InitProvider>
+        </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
