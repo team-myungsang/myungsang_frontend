ea923ba50c7922454091907fdd3b5bb9bdbff9d2
curlyjun <tjdwns4850@gmail.com>
2022-05-04 23:14:18 +0900

    feat: Fix movie page path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { PATH } from '@constants/path';
 import LikeListPage from '@pages/LikeListPage';
 import LoginPage from '@pages/loginPage/LoginPage';
-import MainPage from '@pages/MainPage';
+import MainPage from '@pages/mainPage/MainPage';
 import MyVideoPage from '@pages/MyVideoPage';
 import ProfilePage from '@pages/ProfilePage';
 import SignUpPage from '@pages/SignUpPage';
@@ -14,7 +14,7 @@ function App() {
       <Route path={PATH.MAIN} element={<MainPage />} />
       <Route path={PATH.LOGIN} element={<LoginPage />} />
       <Route path={PATH.SIGNUP} element={<SignUpPage />} />
-      <Route path={PATH.WATCH} element={<WatchPage />} />
+      <Route path={`${PATH.WATCH}:movieId`} element={<WatchPage />} />
       <Route path={PATH.LIKE_LIST} element={<LikeListPage />} />
       <Route path={PATH.MY_VIDEO} element={<MyVideoPage />} />
       <Route path={PATH.PROFILE} element={<ProfilePage />} />
