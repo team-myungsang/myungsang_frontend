8cc5c159cafabf527484071e8e933751b436a538
curlyjun <tjdwns4850@gmail.com>
2022-05-04 21:10:48 +0900

    style: Update font

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    font-family: Pretendard -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
     -webkit-font-smoothing: antialiased;
