1e041ecd6e5ac94094a6007a17d930f8c29e1f8b
curlyjun <tjdwns4850@gmail.com>
2022-05-09 18:00:56 +0900

    feat: Update MainPage

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,13 +1,26 @@
 import FeedItem from '@components/feedItem/FeedItem';
+import Header from '@components/header/Header';
 import { mockFeed } from '@mocks/feed';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { SMainPageWrapper } from './MainPage.style';
 
 function MainPage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const sp = new URLSearchParams(location.search);
+    const category = sp.get('c');
+    /** @todo call api */
+    console.log(category);
+  }, [location.search]);
+
   return (
     <SMainPageWrapper>
-      <FeedItem feed={mockFeed} />
-      <FeedItem feed={mockFeed} />
-      <FeedItem feed={mockFeed} />
+      <Header />
+      {Array.from(Array(10)).map((_, i) => (
+        <FeedItem key="feed" feed={mockFeed} />
+      ))}
     </SMainPageWrapper>
   );
 }
